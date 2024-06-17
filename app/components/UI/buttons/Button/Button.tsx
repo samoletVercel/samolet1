76cce26fc5bb350c587ef49a2148eb43184a7da9
Peter <pkharitonov1@gmail.com>
2024-06-17 08:19:08 +0300

    asd

--- app/components/UI/buttons/Button/Button.tsx
+++ app/components/UI/buttons/Button/Button.tsx
@@ -8,30 +8,12 @@ import { ptRootUIMed } from "@/app/fonts";
 import variables from "@/app/variables.module.scss";
 
 const Button = ({ text, link }: { text: string; link: string }) => {
-  const [isHover, setIsHover] = useState(false);
-
   return (
     <Link
       href={link}
       className={`${ptRootUIMed.className} ${variables.textMain} ${styles.button}`}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
     >
-      <motion.div
-        variants={{
-          active: {
-            width: "1.3rem",
-            backgroundColor: "#ff3129",
-          },
-          inactive: {
-            width: "1rem",
-            backgroundColor: "#333333",
-          },
-        }}
-        initial="inactive"
-        animate={isHover ? "active" : "inactive"}
-        transition={{ duration: 0.2, ease: [0.16, 1, 0.3, 1] }}
-      ></motion.div>
+      <div></div>
       {text}
     </Link>
   );
